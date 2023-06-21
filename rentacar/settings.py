ed2186064a76f1a4df0e07bdafe614c2219f6f7c
kkyeongrokk <sisiko8@naver.com>
2023-06-21 09:30:33 -0700

    Config deployment

--- rentacar/settings.py
+++ rentacar/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import django_on_heroku
 from pathlib import Path
 import os
 import environ
@@ -132,3 +133,5 @@ LOGOUT_REDIRECT_URL = '/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_on_heroku.settings(locals(), databases=False)
